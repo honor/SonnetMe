98b65332cfbdadc75d2323748f676141de486e67
honor <katalina@voteseed.com>
2013-08-22 12:03:23 -0400

    Ass StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SonnetMe::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/more"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
